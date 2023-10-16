3e27fcb14508438e6e79c3db78a6fda6ad381e4e
Mateusz Bielowka <mbielowka@student.agh.edu.pl>
2023-10-16 20:50:39 +0200

    Drugi plik

---
+++ 2.py
@@ -0,0 +1,13 @@
+print("Operatory:")
+potega = 2 ** 3
+print("Potega 2^3 = ", potega)
+bezreszty = 5 // 2
+print("Dzielenie bez reszty 5//2 = ", bezreszty)
+x = 5
+print(x)
+x += 1
+print(x)
+
+a = input("Podaj 1. liczbe:")
+b = input("Podaj 2. liczbe:")
+print(int(a)+int(b))
